e837f24b2e10acf6a8e5152ebbfe7f54abe8fa30
Guti-f <gutialonso20032003@gmail.com>
2026-06-02 22:28:35 -0400

    Add files via upload

---
+++ gutibackend/src/main/java/com/colegio/backend/entrevista/EntrevistaRepository.java
@@ -0,0 +1,6 @@
+package com.colegio.backend.entrevista;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EntrevistaRepository extends JpaRepository<Entrevista, Long> {
+}
