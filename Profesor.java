accee346f83030de8a1939dd01ce4e79fcab8261
Guti-f <gutialonso20032003@gmail.com>
2026-06-02 21:51:59 -0400

    Delete Profesor.java

--- Profesor.java
+++
@@ -1,6 +0,0 @@
-package com.colegio.backend.apoderado;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApoderadoRepository extends JpaRepository<Apoderado, Long> {
-}
