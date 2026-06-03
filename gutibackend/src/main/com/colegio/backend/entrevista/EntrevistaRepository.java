a2183d546d5c8f095c8e8118a65d1222ad05eabb
Guti-f <gutialonso20032003@gmail.com>
2026-06-02 22:06:23 -0400

    Delete gutibackend/src/main/com directory

--- gutibackend/src/main/com/colegio/backend/entrevista/EntrevistaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.colegio.backend.entrevista;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EntrevistaRepository extends JpaRepository<Entrevista, Long> {
-}
--- gutibackend/src/main/com/colegio/backend/profesor/ProfesorRepository.java
+++
@@ -1,6 +0,0 @@
-package com.colegio.backend.profesor;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfesorRepository extends JpaRepository<Profesor, Long> {
-}
