0916679dd8859a7e980d8b7f326a602278dc3581
Guti-f <gutialonso20032003@gmail.com>
2026-06-02 22:29:25 -0400

    Add files via upload

---
+++ gutibackend/src/main/java/com/colegio/backend/profesor/ProfesorRepository.java
@@ -0,0 +1,6 @@
+package com.colegio.backend.profesor;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfesorRepository extends JpaRepository<Profesor, Long> {
+}
