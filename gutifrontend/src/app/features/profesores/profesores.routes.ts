40e2188cd6be9c093f1e4fc74dd116c47c8de276
Guti-f <gutialonso20032003@gmail.com>
2026-06-09 23:33:16 -0400

    Create profesores.routes.ts

---
+++ gutifrontend/src/app/features/profesores/profesores.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { ProfesorPage } from './pages/profesor-page';
+
+export const PROFESORES_ROUTES: Routes = [
+  { path: '', component: ProfesorPage }
+];
