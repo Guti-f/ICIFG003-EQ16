0f7ffb429a9b1b1b3c365290a158c6724149fe69
Guti-f <gutialonso20032003@gmail.com>
2026-06-09 23:24:30 -0400

    Create entrevistas.routes.ts

---
+++ gutifrontend/src/app/features/entrevistas/entrevistas.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { EntrevistaPage } from './pages/entrevista-page';
+
+export const ENTREVISTAS_ROUTES: Routes = [
+  { path: '', component: EntrevistaPage }
+];
