202e4f9e7651be30d1a278bfc2fa3b1863d0c2e2
Guti-f <gutialonso20032003@gmail.com>
2026-06-09 23:12:23 -0400

    Create apoderados.routes.ts

---
+++ gutifrontend/src/app/features/apoderados/apoderados.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { ApoderadoPage } from './pages/apoderado-page';
+
+export const APODERADOS_ROUTES: Routes = [
+  { path: '', component: ApoderadoPage }
+];
